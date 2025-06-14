36cca54803ca139f6d65adc5fe069c9b4f44425f
Mousynano <104660173+Mousynano@users.noreply.github.com>
2025-06-14 19:16:00 +0700

    feat: defined sport in elements

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import { Route, Routes, useLocation } from "react-router-dom";
 import Header from "./components/Header";
 import NotFound from "./components/NotFound";
 import ProtectedRoute from "./components/ProtectedRoute";
-import { fetchData } from "./lib/fetchData";
 
 import Jump from './pages/Exercises/Jump';
 import Pushup from './pages/Exercises/Pushup';
@@ -52,31 +51,31 @@ const App = () => {
 
           <Route path="/exercises/pushup" element={
             <ProtectedRoute>
-              <Pushup fetchData={fetchData} />
+              <Pushup />
             </ProtectedRoute>
           } />
 
           <Route path="/exercises/situp" element={
             <ProtectedRoute>
-              <Situp fetchData={fetchData} />
+              <Situp />
             </ProtectedRoute>
           } />
 
           <Route path="/exercises/squat" element={
             <ProtectedRoute>
-              <Squat fetchData={fetchData} />
+              <Squat />
             </ProtectedRoute>
           } />
 
           <Route path="/exercises/jump" element={
             <ProtectedRoute>
-              <Jump fetchData={fetchData} />
+              <Jump />
             </ProtectedRoute>
           } />
 
           <Route path="/exercises/punch" element={
             <ProtectedRoute>
-              <Punch fetchData={fetchData} />
+              <Punch />
             </ProtectedRoute>
           } />
 
